7baf6e0b30c328eecd6cee2c38a736c1943220ca
swd-d <swdchw@gmail.com>
2022-06-29 17:00:32 +0500

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+# back-up
+
+! check config before start !
+
+install: 
+```npm i```
+start: 
+```node .```
---
+++ cfg.js
@@ -0,0 +1,9 @@
+module.exports = {
+    pathTo: 'D:\\testFolder', // Path to backup:
+    // ** \ = \\
+    // Windows: D:\\testFolder
+    // Linux: /path_to/folder/
+    timeToNextBackUp: 1000, // 86400000 - day, * time in ms *
+    deleteOldBackups: true, 
+    CyclesToDeleteOldBackups: 5, // if deleteOldBackups === false don't work
+}
