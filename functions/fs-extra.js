7db67b6e8408cc5888f2ae76aaa0b649831e71c5
swd-d <swdchw@gmail.com>
2022-07-10 23:14:05 +0500

    !!! TEST UPDATE !!!

--- functions/fs-extra.js
+++ functions/fs-extra.js
@@ -1,14 +1,9 @@
-module.exports = async () =>{
-    const sleep = require('util').promisify(setTimeout);
-    try {
-       require('fs-extra')
-    } catch { 
-        console.log('installing fs-extra..')
-        require('child_process').exec('npm i fs-extra', (e, s) => {
-            if(e) console.log(e);
-            console.log(s)
-        })
-    }
-
-    await sleep(10000)
+try {
+   require('fs-extra')
+} catch { 
+    console.log('installing fs-extra..')
+    require('child_process').exec('npm i fs-extra', (e, s) => {
+        if(e) console.log(e);
+        console.log(s)
+    })
 }
--- index.js
+++ index.js
@@ -1,21 +1,27 @@
-
-require('./functions/fs-extra.js')()
-
-require('./functions/github-update.js')
+const sleep = require('util').promisify(setTimeout);
 
 let cfg = require('./cfg.js');
 
-try {
 if(!cfg.timeToNextBackUp || !cfg.pathTo || !cfg.deleteOldBackups || !cfg.CyclesToDeleteOldBackups) return console.error('[config] don\'t exist timeToNextBackUp, pathTo, deleteOldBackups or CyclesToDeleteOldBackups');
 if(typeof(cfg.timeToNextBackUp) != 'number') return console.error('[config] type timeToNextBackUp != number');
 if(typeof(cfg.pathTo) != 'string') return console.error('[config] type pathTo != string');
-if(cfg.pathTo === `D:\\testFolder`) return new Error('default path, please check configs')
+if(cfg.pathTo === `D:\\testFolder`) return console.error('default path, please check configs')
 if(typeof(cfg.deleteOldBackups) != 'boolean') return console.error('[config] deleteOldBackups != boolean');
 if(typeof(cfg.CyclesToDeleteOldBackups) != 'number') return console.error('[config] CyclesToDeleteOldBackups != number');
-} catch(e) {
-    console.log(e)
 
-}
 console.log('[PASS] config checks');
 
-require('./functions/main.js')()
+async function start() {
+
+    require('./functions/fs-extra.js')()
+
+    await sleep(5000)
+
+    require('./functions/github-update.js')
+
+    await sleep(5000)
+
+    require('./functions/main.js')()
+}
+
+start()
